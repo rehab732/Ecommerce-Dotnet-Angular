7e87b24401c595a544616c4bcb485823517ec98d
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-14 13:05:06 +0300

    create store context

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		[HttpGet]
+		public string GetProducts()
+		{
+			return "This Method wil get all products";
+		}
+
+		[HttpGet("{id}")]
+		public string GetProducts(int id)
+		{
+			return "This Method wil get product by id";
+		}
+	}
+}
